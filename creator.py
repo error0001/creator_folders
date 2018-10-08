ba4577ec4150921fc914f0d9279e78b6ead18cf4
error0001 <error00010001@gmail.com>
2018-10-08 23:16:48 +0300

    clear realization

--- creator.py
+++ creator.py
@@ -1,12 +1,13 @@
 import os
 
-def CreateStrProj(inNameProj):
+def CreateStrProj():
   print("Give a name of project: ")
+  inNameProj = input()
   try:
-    if not os.path.exists(inNameProj):
+    if not os.path.exists('./' + inNameProj + '/'):
       os.makedirs(inNameProj)
   except OSError:
     print('Error: Creating folders of project. ' + inNameProj)
 
 if __name__ == '__main__':
-  CreateStrProj('./data/')
+  CreateStrProj()
