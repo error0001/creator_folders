4c477265f4885c6110b8769a5f8f637d468a019e
error0001 <error00010001@gmail.com>
2018-10-08 23:46:08 +0300

    version 0.1

--- creator.py
+++ creator.py
@@ -6,6 +6,16 @@ def CreateStrProj():
   try:
     if not os.path.exists('./' + inNameProj + '/'):
       os.makedirs(inNameProj)
+    os.chdir(inNameProj)
+    os.makedirs('1_pojects')
+    os.makedirs('2_documentation')
+    os.makedirs('3_cs_structs')
+    os.makedirs('4_uml_structs')
+    os.makedirs('5_addons_soft')
+    os.makedirs('6_other')
+    os.chdir('1_pojects')
+    os.makedirs('current')
+    os.makedirs('old')
   except OSError:
     print('Error: Creating folders of project. ' + inNameProj)
 
