7ac7476a124db7903cbd9061a24a17a4d1d6bd5a
error0001 <error00010001@gmail.com>
2018-10-11 09:22:54 +0300

    forward, delete  line of create files

--- creator.py
+++ creator.py
@@ -7,25 +7,24 @@ def CreateStrProj():
     if not os.path.exists('./' + inNameProj + '/'):
       os.makedirs(inNameProj)
     os.chdir(inNameProj)
-    #walk in the directory, for future
-    #*********************************
-    #from_dir = input()  # get dir
-    #print("***************************************")
-    #for crtgs in os.walk(from_dir):  # Generate a cortages
-	  #  print (crtgs)                  # and print whole
-    #print("***************************************")
+    # Create folders
     os.makedirs('_poject')
     os.makedirs('_documentation')
     os.makedirs('_cs_structs')
     os.makedirs('_uml_structs')
     os.makedirs('_addons_soft')
     os.makedirs('_other')
-    os.chdir('_pojects')
-    file = open("inNameProj" + ".py","w")
-    file.write("import os")
-    file.close
   except OSError:
     print('Error: Creating folders of project. ' + inNameProj)
 
 if __name__ == '__main__':
   CreateStrProj()
+
+
+#walk in the directory, for future
+#*********************************
+#from_dir = input()  # get dir
+#print("***************************************")
+#for crtgs in os.walk(from_dir):  # Generate a cortages
+#  print (crtgs)                  # and print whole
+#print("***************************************")
